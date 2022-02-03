74052d2d74d5c47616847b0a9a9c4eed3465067d
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-02-03 10:35:31 -0600

    Fixed issue with ttyd that caused it to drop connections

--- ttyd/src/main.rs
+++ ttyd/src/main.rs
@@ -1,36 +1,36 @@
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	eprintln!("Begin");
-	let width: usize = std::env::var("WIDTH")
-		.unwrap_or("80".to_string())
-		.parse()
-		.unwrap();
+	let width: usize = std::env::var("WIDTH").unwrap_or("80".to_string()).parse()?;
 	let tab_width: usize = std::env::var("TAB_WIDTH")
 		.unwrap_or("4".to_string())
-		.parse()
-		.unwrap();
+		.parse()?;
 	let listen_addr = std::env::var("LISTEN").unwrap_or("0.0.0.0:23".to_string());
 	let post_url = std::env::var("POST_URL").unwrap_or("http://pland/txt".to_string());
-	let listener = TcpListener::bind(listen_addr)
-		.await
-		.expect("Couldn't listen");
+	let listener = TcpListener::bind(listen_addr).await?;
 	let mut x = 0;
 	let client = reqwest::Client::new();
 	while let Ok(incoming) = listener.accept().await {
-		async {
+		let _result: Result<(), Box<dyn std::error::Error>> = async {
 			let incoming = incoming.0;
-			eprintln!("{} opened", incoming.peer_addr().unwrap());
-			let mut buf = [0; 1024];
-			while let Ok(n) = incoming.try_read(&mut buf) {
-				if n == 0 {
-					continue;
-				}
+			eprintln!("{} opened", incoming.peer_addr()?);
+			let mut buf = [0u8; 1024];
+			loop {
+				incoming.readable().await?;
+				let n = match incoming.try_read(&mut buf) {
+					Ok(0) => break,
+					Ok(n) => n,
+					Err(ref e) if e.kind() == tokio::io::ErrorKind::WouldBlock => {
+						continue;
+					}
+					Err(_) => break,
+				};
 				let to_send: String = buf[..n]
 					.iter()
 					// Convert to chars
-					.map(|&x| std::char::from_u32(x as u32).unwrap())
+					.flat_map(|&x| std::char::from_u32(x as u32))
 					// Sanitize
 					.flat_map(|c| match c {
 						c if c.is_ascii_alphanumeric() => Some(c.to_string()),
@@ -52,10 +52,13 @@ async fn main() {
 					})
 					.collect();
 				print!("{}", to_send);
-				client.post(&post_url).body(to_send).send().await.unwrap();
+				client.post(&post_url).body(to_send).send().await?;
 			}
-			eprintln!("{} closed", incoming.peer_addr().unwrap());
+			eprintln!("{} closed", incoming.peer_addr()?);
+			Ok(())
 		}
-		.await
+		.await;
+		_result?
 	}
+	Ok(())
 }
